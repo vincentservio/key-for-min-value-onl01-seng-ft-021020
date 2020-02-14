1c686a9afa77b7ddc0bae3883cd4e8d4eb9b1cc2
Vincent Servio <vincentservio@gmail.com>
2020-02-14 02:21:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require "pry"
 def key_for_min_value(name_hash)
-
+  
+name_hash.collect
 end
+# => {:blake=>500, :ashley=>2, :adam=>1}
