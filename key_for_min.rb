6bfd6e414b3fc5ebedca566147d61b8a11915583
Vincent Servio <vincentservio@gmail.com>
2020-02-14 02:23:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 require "pry"
 def key_for_min_value(name_hash)
-  
-name_hash.collect
+ lowest_key = nil 
+ lowest_value = nil 
+ hash.each do |name,num|
+   if lowest_value == nill || value < lowest_value
+     lowest_value = value
+     lowest_key = key 
+   end 
+ end 
+ lowest_key
 end
 # => {:blake=>500, :ashley=>2, :adam=>1}
